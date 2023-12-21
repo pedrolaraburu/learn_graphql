3f89f8ac8b034156e1e0d183a622dacc31b2e4c1
Pedro <pedrolaraburu@gmail.com>
2023-12-21 10:31:42 -0300

    ✨feat: add user authentication and password hashing

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -8,13 +8,24 @@ export default {
     Query : {
         users: () => prisma.user.findMany(),
         user: async (_: any, { id }: any) => {
-            const userid = parseInt(id);
-            prisma.user.findUnique({ where: { id: userid } })
+            const user = prisma.user.findUnique({ where: { id } });
+            if (!user) {
+                throw new Error('User not found');
+            }
         },
     }, 
     Mutation: {
         createUser: (_: any, args: any) => {
             return userService.createUser(args);
+        },
+        updateUser: (_: any, args: any) => {
+            return userService.updateUser(args);
+        },
+        deleteUser: (_: any, args: any) => {
+            return userService.deleteUser(args);
+        },
+        login(_ : any, args: any) {
+            return userService.login(args);
         }
     }
 }
--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import JwtService from "./JwtService";
+import bcrypt from 'bcrypt';
 export default class UserService {
     prisma = new PrismaClient();
+    jwt = new JwtService();
 
     public async createUser(args: { email: string; name: string; password: string; }) {
         const { email, name, password } = args;
         try {
+            const hashedPassword = await bcrypt.hash(password, 10);
             const newUser = await this.prisma.user.create({
                 data: {
                     email,
                     name,
-                    password,
+                    password: hashedPassword,
                 },
             });
             return newUser;
@@ -20,5 +24,74 @@ export default class UserService {
                 throw new Error('An unknown error occurred');
             }
         }
+    };
+    public async updateUser(args: { id: number; email: string; name: string; password: string; }) {
+        const { id, email, name, password } = args;
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const updatedUser = await this.prisma.user.update({
+                where: { id },
+                data: {
+                    email,
+                    name,
+                    password: hashedPassword,
+                },
+            });
+            if(!updatedUser) {
+                throw new Error('User not found');
+            }
+            return updatedUser;
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            } else {
+                throw new Error('An unknown error occurred');
+            }
+        }
+    };
+
+    public async deleteUser(args: { id: number; }) {
+        const { id } = args;
+        try {
+            await this.prisma.user.delete({
+                where: { id },
+            });
+            return true; 
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            } else {
+                throw new Error('An unknown error occurred');
+            }
+        }
+    }
+    
+    public async login(args: { email: string; password: string }) {
+        const { email, password } = args;
+        try {
+            const user = await this.prisma.user.findUnique({
+                where: { email },
+            });
+
+            if (!user) {
+                throw new Error('User not found');
+            }
+
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (!passwordMatch) {
+                throw new Error('Invalid password');
+            }
+
+            const token = this.jwt.generateToken({ id: user.id, email: user.email });
+            return token;
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            } else {
+                throw new Error('An unknown error occurred');
+            }
+        }
     }
 }
+

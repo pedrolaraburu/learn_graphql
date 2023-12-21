52ba52d1ae4d3d85b7bb0eb95584dba4e82a1b7a
Pedro <pedrolaraburu@gmail.com>
2023-12-21 10:34:54 -0300

    ✨feat: change to apollo-server-express and apply authDirective

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
-import {ApolloServer} from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './graphql/mergeSchemas';
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import authDirectiveTransformer from './graphql/directives/authDirective';
+import express from 'express';
+const PORT = 4000;
 
-const server = new ApolloServer({typeDefs, resolvers});
+const app = express();
 
-server.listen().then(({url}) => {
-  console.log(`🚀 Server ready at ${url}`);
+let schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
 });
+
+// Add the auth directive to the schema
+schema = authDirectiveTransformer(schema, 'auth');
+
+const runServer = async () => {
+  const server = new ApolloServer({ 
+    schema,
+    context: ({ req }) => ({ req }), 
+  });
+  await server.start();
+  app.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+  });
+  server.applyMiddleware({ app });  
+}
+
+runServer();
